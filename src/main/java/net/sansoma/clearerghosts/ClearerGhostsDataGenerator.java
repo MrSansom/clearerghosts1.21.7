97071f13e2445c06f09a74631da58c86ab3136e6
MrSansom <mrsansoms@gmail.com>
2025-07-15 20:15:48 +0100

    Initial commit

---
+++ src/main/java/net/sansoma/clearerghosts/ClearerGhostsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sansoma.clearerghosts;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ClearerGhostsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
